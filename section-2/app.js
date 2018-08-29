58d4b7aca819b9561500c20ceb62624d6f99072f
Fahad Qazi <fqazi@oxgene.com>
2018-08-30 00:58:35 +0100

    logging use eventemitter

--- section-2/app.js
+++ section-2/app.js
@@ -1,8 +1,17 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const logger = require('./logger')
 
-emitter.on('messageLogged', function(){
-    console.log('Listener called')
+// Register Event listener
+emitter.on('messageLogged', function(arg){
+    console.log('Listener called: ', arg)
+})
+// Raise an event
+emitter.emit('messageLogged', {id: 1, url:'http://'})
+
+emitter.on('logging', function(arg){
+    logger.log(arg.data)
+    console.log('Message logged')
 })
 
-emitter.emit('messageLogged')
+emitter.emit('logging', {data: 'hello'})
