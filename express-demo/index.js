7c47efdad3172583a8c3d951b844ee5d5a80e159
Fahad Qazi <fqazi@oxgene.com>
2018-08-31 00:29:54 +0100

    http delete request

--- express-demo/index.js
+++ express-demo/index.js
@@ -22,17 +22,14 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
     const courseId = req.params.id;
     const course = courses.find(c => c.id === parseInt(courseId));
-    if (!course) res.status(404).send("Request with given id not found");
+    if (!course) return res.status(404).send("Request with given id not found");
     res.send(course);
 });
 
 app.post("/api/courses/", (req, res) => {
     const result = validateCourse(req.body)
 
-    if (result.error) {
-        res.status(400).send(result.error);
-        return;
-    }
+    if (result.error) return res.status(400).send(result.error);
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -45,22 +42,33 @@ app.put("/api/courses/:id", (req, res) => {
     //lookup course
     //if not return 404
     var course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("Course not found");
+    if (!course) return res.status(404).send("Course not found");
 
     //validate input
     //if invalid return 404
     const result = validateCourse(req.body);
 
-    if (result.error) {
-        res.status(404).send(result.error);
-        return;
-    }
+    if (result.error) return res.status(404).send(result.error);
 
     //update course
     //return udpated course
     course.name = req.body.name;
     res.send(course);
 });
+
+app.delete('/api/courses/:id', (req, res) =>{
+    //look up course    
+    //if doesn't exit, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) return res.status(404).send('Course with that id not found')
+
+    // delete
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+
+    //return same course
+    res.send(course)
+})
 
 app.listen(port, err => {
     if (err) throw err;
