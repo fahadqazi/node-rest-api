c7c6816d002ea137d77554fd8899a9efcbb9e537
Fahad Qazi <fqazi@oxgene.com>
2018-08-30 23:55:54 +0100

    use Joi for input validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,10 +27,14 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses/", (req, res) => {
-    if (!req.body.name || req.body.name.length < 3) {
-        res.status(400).send(
-            "Name is required and should be minimum 3 characters"
-        );
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema)
+
+    if (result.error) {
+        res.status(400).send(result.error);
         return;
     }
     const course = {
