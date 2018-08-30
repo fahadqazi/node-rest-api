f96783a53be41ad75a37cd958cd9d77ada131e06
Fahad Qazi <fqazi@oxgene.com>
2018-08-30 23:45:07 +0100

    basic post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'}
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
 ]
 
 app.get('/', (req, res) => {
@@ -18,13 +20,22 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const courseId = req.params.id
-    const course = courses.find((c)=> c.id === parseInt(courseId))
+    const course = courses.find((c) => c.id === parseInt(courseId))
     if (!course) res.status(404).send('Request with given id not found')
     res.send(course)
 })
 
+app.post('/api/courses/', (req, res) => {
+    const course ={
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
 app.listen(port, (err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log(`listening on port ${port}`)
 })
 
