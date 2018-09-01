2d6c27f0caf24e496061dd885d7a9130f3d28ba3
Fahad Qazi <fqazi@oxgene.com>
2018-09-01 15:12:37 +0100

    adding public folder and static middleware to serve up content from public folder

--- express-demo/index.js
+++ express-demo/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); //key=value&key=value
+app.use(express.static('public'))
 
 app.use(logger)
 app.use(auth)
