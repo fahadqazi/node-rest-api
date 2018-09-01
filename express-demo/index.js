9d644210581351ed8e3bd9ff17753942ae2596ec
Fahad Qazi <fqazi@oxgene.com>
2018-09-01 15:10:18 +0100

    now able to send url-encoded messages

--- express-demo/index.js
+++ express-demo/index.js
@@ -6,6 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true})); //key=value&key=value
+
 app.use(logger)
 app.use(auth)
 
