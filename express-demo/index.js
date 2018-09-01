4c1f76f91b98d976ccc50e0037390060b5ce5885
Fahad Qazi <fqazi@oxgene.com>
2018-09-01 17:43:45 +0100

    refactored /api/coures/ routes to separate file and used router instead of app to export routes to app

--- express-demo/index.js
+++ express-demo/index.js
@@ -6,6 +6,7 @@ const Joi = require("joi");
 const logger = require("./logger");
 const auth = require("./authenticate");
 const express = require("express");
+const courses =  require('./routes/courses')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true})); //key=value&key=value
 app.use(express.static("public"));
 app.use(helmet());
+
+// For any routes begining with /api/courses use the 'courses' router
+app.use('/api/courses', courses)
 
 // Logging the environment
 debug('Application name: ' + config.get('name'))
@@ -34,69 +38,8 @@ if (app.get("env") === "development") {
 app.use(logger);
 app.use(auth);
 
-const courses = [
-    {id: 1, name: "course1"},
-    {id: 2, name: "course2"},
-    {id: 3, name: "course3"}
-];
-
 app.get("/", (req, res) => {
     res.render('index.pug', {title: 'My Express App', message:'Hello world'})
-});
-
-app.get("/api/courses", (req, res) => {
-    res.send(courses);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-    const courseId = req.params.id;
-    const course = courses.find(c => c.id === parseInt(courseId));
-    if (!course) return res.status(404).send("Request with given id not found");
-    res.send(course);
-});
-
-app.post("/api/courses/", (req, res) => {
-    const result = validateCourse(req.body);
-
-    if (result.error) return res.status(400).send(result.error);
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-    //lookup course
-    //if not return 404
-    var course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("Course not found");
-
-    //validate input
-    //if invalid return 404
-    const result = validateCourse(req.body);
-
-    if (result.error) return res.status(404).send(result.error);
-
-    //update course
-    //return udpated course
-    course.name = req.body.name;
-    res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-    //look up course
-    //if doesn't exit, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("Course with that id not found");
-
-    // delete
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    //return same course
-    res.send(course);
 });
 
 app.listen(port, err => {
@@ -104,11 +47,4 @@ app.listen(port, err => {
     debug(`listening on port ${port}`);
 });
 
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string()
-            .min(3)
-            .required()
-    };
-    return Joi.validate(course, schema);
-}
+
