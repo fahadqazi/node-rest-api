90fa53daea219360b53986a2eb5e4725a8acddd6
Fahad Qazi <fqazi@oxgene.com>
2018-09-02 15:59:04 +0100

    added template engine - home page

--- 4.14- Project- Genres Endpoint/vidly/index.js
+++ 4.14- Project- Genres Endpoint/vidly/index.js
@@ -6,8 +6,12 @@ const genres = require('./routes/genres')
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.use(express.json());
 app.use(express.urlencoded({entended: true}))
+app.use(express.static('public'))
 
 app.use('/api/genres', genres)
 app.use('/', home)
