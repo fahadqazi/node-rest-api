a469890029130dcc70852c394dd9c5f224d84cdc
Fahad Qazi <fqazi@oxgene.com>
2018-09-02 21:30:47 +0100

    learning promises

--- async-patterns/index.js
+++ async-patterns/index.js
@@ -19,35 +19,35 @@ function getUser(id){
 // 1. Callbacks
 
 function callbackMain() {
-    console.log("Before");
-    getUser(1, function (user) {
-        console.log("User: ", user);
-        getRepos(user.gitHubUsername, function (repos) {
-            console.log("Repos: ", repos);
-            getRepoStars(repos[0], function (stars) {
-                console.log("Stars: ", stars);
-            });
-        });
+  console.log("Before");
+  getUser(1, function(user) {
+    console.log("User: ", user);
+    getRepos(user.gitHubUsername, function(repos) {
+      console.log("Repos: ", repos);
+      getRepoStars(repos[0], function(stars) {
+        console.log("Stars: ", stars);
+      });
     });
-    console.log("After");
+  });
+  console.log("After");
 }
 
 function getUser(id, callback) {
-    setTimeout(() => {
-        callback({id: id, gitHubUsername: "fahad"});
-    }, 2000);
+  setTimeout(() => {
+    callback({ id: id, gitHubUsername: "fahad" });
+  }, 2000);
 }
 
 function getRepos(username, callback) {
-    setTimeout(() => {
-        callback(["repo1", "repo2", "repo3"]);
-    }, 2000);
+  setTimeout(() => {
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
 
 function getRepoStars(repoName, callback) {
-    setTimeout(() => {
-        callback(3);
-    }, 2000);
+  setTimeout(() => {
+    callback(3);
+  }, 2000);
 }
 
 // callbackMain();
@@ -55,41 +55,41 @@ function getRepoStars(repoName, callback) {
 // 2. Fixing callback hell using named functions instead of anonymous functions
 
 function fixCallbackHellMain() {
-    console.log("Before");
-    getUser(1, displayUser);
-    console.log("After");
+  console.log("Before");
+  getUser(1, displayUser);
+  console.log("After");
 }
 
 function displayStars(stars) {
-    console.log("Stars: ", stars);
+  console.log("Stars: ", stars);
 }
 
 function displayRepos(repos) {
-    console.log("Repos: ", repos);
-    getRepoStars(repos, displayStars);
+  console.log("Repos: ", repos);
+  getRepoStars(repos, displayStars);
 }
 
 function displayUser(user) {
-    console.log("User: ", user);
-    getRepos(user, displayRepos);
+  console.log("User: ", user);
+  getRepos(user, displayRepos);
 }
 
 function getUser(id, callback) {
-    setTimeout(() => {
-        callback({id: id, gitHubUsername: "fahad"});
-    }, 2000);
+  setTimeout(() => {
+    callback({ id: id, gitHubUsername: "fahad" });
+  }, 2000);
 }
 
 function getRepos(user, callback) {
-    setTimeout(() => {
-        callback(["repo1", "repo2", "repo3"]);
-    }, 2000);
+  setTimeout(() => {
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
 
 function getRepoStars(repo, callback) {
-    setTimeout(() => {
-        callback(3);
-    }, 2000);
+  setTimeout(() => {
+    callback(3);
+  }, 2000);
 }
 
 fixCallbackHellMain();
