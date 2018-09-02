8e8c9d14f802cdaa3afea0e4fe4933cc6094a2f9
Fahad Qazi <fqazi@oxgene.com>
2018-09-02 19:24:01 +0100

    prettier

--- async-patterns/index.js
+++ async-patterns/index.js
@@ -18,26 +18,27 @@ function getUser(id){
 
 // 1. Callbacks
 
-function callbackMain(){
-  console.log('Before')
-  getUser(1, function(user){
-      console.log('User: ', user)
-      getRepos(user.gitHubUsername, function(repos){
-          console.log('Repos: ', repos)
-      })
-  })
-  console.log('After')
+function callbackMain() {
+    console.log("Before");
+    getUser(1, function (user) {
+        console.log("User: ", user);
+        getRepos(user.gitHubUsername, function (repos) {
+            console.log("Repos: ", repos);
+        });
+    });
+    console.log("After");
 }
 
-function getUser(id, callback){
-  setTimeout(() => {
-    callback({id: id, gitHubUsername: 'fahad'})
-  }, 2000)
+function getUser(id, callback) {
+    setTimeout(() => {
+        callback({id: id, gitHubUsername: "fahad"});
+    }, 2000);
 }
 
-function getRepos(username, callback){
-    setTimeout(()=>{
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000)
+function getRepos(username, callback) {
+    setTimeout(() => {
+        callback(["repo1", "repo2", "repo3"]);
+    }, 2000);
 }
+
 callbackMain();
