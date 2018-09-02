21cdd7583c8e9d543fed846994bed69ecac31201
Fahad Qazi <fqazi@oxgene.com>
2018-09-02 19:16:14 +0100

    async using callback

--- async-patterns/index.js
+++ async-patterns/index.js
@@ -1,3 +1,4 @@
+/*
 console.log('Before')
 const user = getUser(1)
 console.log('User: ', user)
@@ -9,4 +10,31 @@ function getUser(id){
       console.log('Reading a user from the database...')
       return {id: id, gitHubUsername: 'fahad'}
   }, 2000)
+}
+*/
+
+// The above code will not work.
+// There are three approaches to fix this.
+
+// 1. Callbacks
+
+console.log('Before')
+getUser(1, function(user){
+    console.log('User: ', user)
+    getRepos(user.gitHubUsername, function(repos){
+        console.log('Repos: ', repos)
+    })
+})
+console.log('After')
+
+function getUser(id, callback){
+  setTimeout(() => {
+    callback({id: id, gitHubUsername: 'fahad'})
+  }, 2000)
+}
+
+function getRepos(username, callback){
+    setTimeout(()=>{
+        callback(['repo1', 'repo2', 'repo3'])
+    }, 2000)
 }
