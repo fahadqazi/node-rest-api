99380a8f568ed1b53addb625617026cd2470db20
Fahad Qazi <fqazi@oxgene.com>
2018-09-02 22:23:27 +0100

    working with resolving promises with all and race

--- async-patterns/promise-api.js
+++ async-patterns/promise-api.js
@@ -1,5 +1,36 @@
+// Resolved promises
+
 const p1 = Promise.resolve({ id: 1 });
 p1.then(result => console.log(result));
 
 const p2 = Promise.reject(new Error("Cannot resolve this one..."));
 p2.catch(err => console.log(err));
+
+// Running promises in parallel
+
+const p3 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log("Async operation one...");
+    resolve(1);
+  }, 2000);
+});
+
+const p4 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log("Async operation two...");
+    resolve(2);
+  }, 2000);
+});
+
+const p5 = new Promise(reject => {
+  setTimeout(() => {
+    reject(new Error("something went wrong"));
+  }, 2000);
+});
+Promise.all([p3, p4, p5])
+  .then(result => console.log("result: ", result))
+  .catch(err => console.log("error: ", err.message));
+
+Promise.race([p3, p4])
+  .then(result => console.log("result: ", result))
+  .catch(err => console.log("err: ", err));
