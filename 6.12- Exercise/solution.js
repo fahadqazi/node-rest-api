2a53ad527a25357d11554a53ce41ea0230a6bf06
Fahad Qazi <fqazi@oxgene.com>
2018-09-08 12:26:26 +0100

    exercise

--- 6.12- Exercise/solution.js
+++ 6.12- Exercise/solution.js
@@ -1,16 +1,3 @@
-
-// getCustomer(1, (customer) => {
-//   console.log('Customer: ', customer);
-//   if (customer.isGold) {
-//     getTopMovies((movies) => {
-//       console.log('Top movies: ', movies);
-//       sendEmail(customer.email, movies, () => {
-//         console.log('Email sent...')
-//       });
-//     });
-//   }
-// });
-
 async function notifyCustomer() {
   const customer = await getCustomer(1);
   console.log('Customer: ', customer);
